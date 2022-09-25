34338fa1ffd590ab3d5db16cfc5f441ee644f958
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-09-25 11:06:31 +0330

    hive_crud

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hive_crud/model_class.dart';
+import 'package:flutter_hive_crud/screens/show_data_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+  Hive.registerAdapter(ModelClassAdapter());
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ShowDataScreen(),
+    );
+  }
+}
